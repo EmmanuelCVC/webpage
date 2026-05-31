2152412a26d2c6d387d1034f641eeaca34ce6f0d
Emmanuel Carballo Vargas <64994528+EmmanuelCVC@users.noreply.github.com>
2026-05-30 23:58:48 -0600

    Update hero-data.js

--- hero-data.js
+++ hero-data.js
@@ -1,6 +1,6 @@
 const heroData = [
     {
-        title: "Diseño e implementación de ingeniería eléctrica y tecnologías de control.",
+        title: "Diseño e implementación de ingeniería <u>eléctrica</u> y tecnologías de <u>control</u>.",
         description: "La electricidad impulsa el desarrollo del mundo. ELECCON es una empresa dedicada a crear soluciones seguras, profesionales y de alta calidad técnica para la sociedad."
     }
 ];
